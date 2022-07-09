5ff191462d6f796bdbaf0c81b08488f73c8ed012
Gian Franco <gian_franco_08@hotmail.com>
2022-07-09 15:53:31 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import Header from "./components/Header";
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      {/* header */}
+      {/* main descripcion yun afoto */}
+      {/* proyectos */}
+      {/* tecnologias */}
+      {/* formulario de contacto */}
+      <header>hola</header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="#proyectos">Proyectos</a>
+          </li>
+          <li>
+            <a href="#contacto">contacto</a>
+          </li>
+          <li>
+            <a href="#">Cv</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
